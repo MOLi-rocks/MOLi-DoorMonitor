4323ff99009e5c372b7f7c190a42d5dfd8fc30bf
Yuer <passerbyid@gmail.com>
2016-04-07 11:53:30 +0800

    監視器位址改設定於 env 之中。

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function writeData(data) {
 
 function getCameraSnapshot(chatId) {
   var options = {
-    url: 'http://163.22.32.59:50080/cgi-bin/wappaint?pic_size=2',
+    url: config.cameraURL,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
     },
